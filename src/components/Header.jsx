d11e10784d40d94d5c361d4ecce3da8e0d38f6fe
Aditya30december2003 <adityasmjain@gmail.com>
2025-01-09 23:07:08 +0530

    made changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -107,7 +107,7 @@ const Header = () => {
             <button onClick={()=>window.open('/Aditya Jain_CV_Resume_NEW_Online.pdf' , '_blank')} className="px-6 py-2 border border-white rounded-full hover:bg-white hover:text-black transition-colors">
               Resume
             </button>
-            <Link href='https://www.fiverr.com/aditya30122003?public_mode=true' className="px-6 py-2 border border-white rounded-full hover:bg-white hover:text-black transition-colors">
+            <Link href='https://www.fiverr.com/aditya30122003?public_mode=true' className="px-6 py-2 border border-white text-center rounded-full hover:bg-white hover:text-black transition-colors">
               Hire Me
             </Link>
           </div>
